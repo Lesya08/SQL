c379f908d42a8288115e569f330c05550a2ed048
Lesya <lesik_il_89@mail.ru>
2023-12-17 21:44:04 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,60 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDataBase;
+
+public class LoginTest {
+    LoginPage loginPage;
+
+    @AfterAll
+    static void tearDownAll() {
+
+        cleanDataBase();
+    }
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void successfulLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void randomUser() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    void registeredUserWithRandomCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+
+}
